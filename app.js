a1e2582a225740168361148dbe8b409a1e1deddc
Caio Duque <caioduque.dev@gmail.com>
2021-03-11 10:12:38 -0300

    Add projects model and routes.

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
-const naversRoute = require('./routes/navers');
+const naversRoute = require("./routes/navers");
+const projectsRouter = require("./routes/projects");
 
 require("dotenv").config();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", naversRoute);
-
-
-
-
-
+app.use("/api", projectsRouter);
 
 module.exports = app;
--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
-
+const Project = require("../models/project");
 
 // (Index) Rota para listagem dos Projetos
+router.get("/projects", async (req, res) => {
+  try {
+    const projects = await Project.find({});
+    res.status(200).json(projects);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
 
 // (Show) Rota para detalhar um projeto
+router.get("/projects/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const project = await Project.findById(id);
+    if (!id) {
+      return res.status(404).send();
+    }
+    res.status(200).json(project);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
 
 // (Store) Rota de Criação de Projeto
+router.post("/project", async (req, res) => {
+  try {
+    const data = req.body;
+    const newProj = new Project(data);
+    await newProj.save();
+    res.status(201).json(newProj);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+
+module.exports = router;
